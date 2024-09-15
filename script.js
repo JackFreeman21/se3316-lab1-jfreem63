dff797c2fa2ea43fdaea58549f72fee9c8a5507c
Jfreem63 <jfreem63@uwo.ca>
2024-09-15 17:28:09 -0400

    Added images foldre and the gitignore file

---
+++ script.js
@@ -0,0 +1,12 @@
+const countries = [
+    { name: "United States", currency: "USD" },
+    { name: "Canada", currency: "CAD" },
+    { name: "United Kingdom", currency: "GBP" },
+    { name: "Germany", currency: "EUR" },
+    { name: "Japan", currency: "JPY" },
+    { name: "Australia", currency: "AUD" },
+    { name: "India", currency: "INR" },
+    { name: "China", currency: "CNY" },
+    { name: "Brazil", currency: "BRL" },
+    { name: "South Africa", currency: "ZAR" }
+  ];
