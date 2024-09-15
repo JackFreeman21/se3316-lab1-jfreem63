ce6d9e024cbd96e98fb6afbdf709c00c2cb7aa8b
Jfreem63 <jfreem63@uwo.ca>
2024-09-15 18:18:50 -0400

    Basic HTML and CSS for 1 plant. Missing images and the list of plant but will add this to next comit

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const countries = [
-    { name: "United States", currency: "USD" },
-    { name: "Canada", currency: "CAD" },
-    { name: "United Kingdom", currency: "GBP" },
-    { name: "Germany", currency: "EUR" },
-    { name: "Japan", currency: "JPY" },
-    { name: "Australia", currency: "AUD" },
-    { name: "India", currency: "INR" },
-    { name: "China", currency: "CNY" },
-    { name: "Brazil", currency: "BRL" },
-    { name: "South Africa", currency: "ZAR" }
+const provincesAndTerritories = [
+    "Alberta",
+    "British Columbia",
+    "Manitoba",
+    "New Brunswick",
+    "Newfoundland and Labrador",
+    "Nova Scotia",
+    "Ontario",
+    "Prince Edward Island",
+    "Quebec",
+    "Saskatchewan",
+    "Northwest Territories",
+    "Nunavut",
+    "Yukon"
   ];
+  
